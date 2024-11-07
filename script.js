cd25a1ae53a7e28b3dad0f5e3fd33b9d4790a459
itsmeneriza <saldoneriza19@gmail.com>
2024-11-07 14:12:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ async function fetchData(){
   try{
 
     const pokemonName= document.getElementById("pokemonName").value.toLowerCase();
-    const response = await fetch(`https://pokepai.co/api/v2/pokemon/${pokemonName}`);
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
